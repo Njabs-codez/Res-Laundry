cbeba6819717b3cef139afe0163e34b840ac3d3b
Njabs-codez <njabsmathonsi52@gmail.com>
2026-04-25 15:42:43 +0200

    Added Models required for the system

---
+++ backend/src/main/java/com/college/backend/Machine.java
@@ -0,0 +1,11 @@
+package com.college.backend;
+
+enum MachineType{
+    DRYER,
+    WASHING
+}
+
+public class Machine {
+    private int number;
+    private MachineType type;
+}
---
+++ backend/src/main/java/com/college/backend/Resident.java
@@ -0,0 +1,10 @@
+package com.college.backend;
+
+
+public class Resident {
+    private int studentNumber;
+    private String phoneNumber;
+    private String firstName;
+    private String LastName;
+    private String roomNumber;
+}
