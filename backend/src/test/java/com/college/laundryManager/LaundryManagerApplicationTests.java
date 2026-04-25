48a719feae9b7cfc49797c06af3044fc23edaa38
Njabs-codez <njabsmathonsi52@gmail.com>
2026-04-25 15:02:04 +0200

    Removed tracking of .idea

---
+++ backend/src/test/java/com/college/laundryManager/LaundryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.laundryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
