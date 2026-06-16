44285cf0aa58832d6c781a18aa853322586a39e7
Njabs-codez <njabsmathonsi52@gmail.com>
2026-06-16 22:34:56 +0200

    Added validation to dtos.

--- backend/src/main/java/com/college/backend/dtos/LoginDTO.java
+++ backend/src/main/java/com/college/backend/dtos/LoginDTO.java
@@ -1,5 +1,16 @@
 package com.college.backend.dtos;
 
-public record LoginDTO() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-}
+public record LoginDTO(
+    @NotNull(message = "Student number is a required field.")
+    @NotBlank(message = "Student number cannot be blank.")
+    Integer studentNumber,
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
+        message = "Password must be at least 8 characters and contain at least one uppercase letter, number, and symbol"
+    )
+    String password
+) {}
--- backend/src/main/java/com/college/backend/dtos/RegisterDTO.java
+++ backend/src/main/java/com/college/backend/dtos/RegisterDTO.java
@@ -1,3 +1,30 @@
 package com.college.backend.dtos;
 
-public record RegisterDTO() {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record RegisterDTO(
+    @NotNull(message = "Student number is a required field.")
+    @NotBlank(message = "Student number cannot be blank.")
+    Integer studentNumber,
+    @NotNull(message = "First name is a required field.")
+    @NotBlank(message = "First name cannot be blank.")
+    @Size(min = 3, message = "First name must be at least 3 characters long.")
+    String firstName,
+    @NotNull(message = "Last name is a required field.")
+    @NotBlank(message = "Last name cannot be blank.")
+    @Size(min = 3, message = "Last name must be at least 3 characters long.")
+    String lastName,
+    @NotNull(message = "Password is a required field.")
+    @NotBlank(message = "Password cannot be blank.")
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
+        message = "Password must be at least 8 characters and contain at least one uppercase letter, number, and symbol"
+    )
+    String password,
+    @NotNull(message = "Room number is a required field.")
+    @NotBlank(message = "Room number cannot be blank.")
+    String roomNumber
+) {}
